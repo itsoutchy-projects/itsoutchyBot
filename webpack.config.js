e6926a1ca18e4dc54d7fbf6e24a4dcb45f47145f
itsoutchy <itsoutchy55@yahoo.com>
2023-12-17 14:55:54 +0000

    add more polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,9 @@ module.exports = {
         "stream": require.resolve("stream-browserify"),
         "assert": require.resolve("assert/"),
         "zlib": require.resolve("browserify-zlib"),
-        "util": require.resolve("util/")
+        "util": require.resolve("util/"),
+        "url": require.resolve("url/"),
+        "console": require.resolve("console-browserify")
     }
   },
   plugins: [

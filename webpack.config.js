a4bd6a76f900ee298a6881acdd9790b3630cc686
itsoutchy <itsoutchy55@yahoo.com>
2023-12-17 14:29:04 +0000

    remove externals

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  externals: {
-    "node:path": "commonjs path",
-    "node:worker_threads": "commonjs path",
-    "node:util": "commonjs path",
-    "node:timers": "commonjs path",
-    "node:timers/promises": "commonjs path",
-    "node:stream": "commonjs path"
-  }
+//   externals: {
+//     "node:path": "commonjs path",
+//     "node:worker_threads": "commonjs path",
+//     "node:util": "commonjs path",
+//     "node:timers": "commonjs path",
+//     "node:timers/promises": "commonjs path",
+//     "node:stream": "commonjs path"
+//   }
 };

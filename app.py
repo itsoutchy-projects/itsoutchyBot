691a00ef96a72a7d1013cef9363a5ec2cf21161f
itsoutchy-projects <itsoutchy55@yahoo.com>
2026-08-11 23:22:48 +0100

    holy shit why did i not commit this its been like nearly 2 years- uhh attempt to add tickets i think and uhh permission locking and stuff uhhhhhhhhhhhhhhhhhhhhhhh

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ import config
 import discord
 import ids
 import embeds
+import discord.app_commands
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+tree = discord.app_commands.CommandTree(client=client)
+
+prefix = "i!"
+
+#create_button = None
 
 @client.event
 async def on_ready():
@@ -39,10 +46,37 @@ async def on_message(msg : discord.Message):
         # music drops channel and MonitoRSS bot
         await msg.publish() # automatically publish notifs, saves me a job
 
+    # commands
+    if msg.content.lower().startswith(f"{prefix}tsetup"):
+        # sets up a message with a button to let you make a ticket
+        tchannel = msg.guild.get_channel(ids.tickets)
+        view = discord.ui.View(timeout=None)
+        global create_button
+        create_button = discord.ui.Button(style=discord.ButtonStyle.primary, label="Create Ticket")
+        view.add_item(create_button)
+        await tchannel.send(content="Press the button to open a ticket, then describe your issue in the channel I send to you", view=view)
+
 @client.event
 async def on_message_delete(msg : discord.Message):
     # hehe you cant delete your messages to get away with doing something naughty >:)
     message_logs = msg.guild.get_channel(ids.message_logs)
     await message_logs.send(embed=embeds.message_delete(msg), silent=True)
+
+@tree.command(name="delete-ticket", description="Deletes the ticket this command is used in. If it's actually a ticket")
+async def delete_ticket_command(interaction : discord.Interaction):
+    if interaction.guild.get_role(ids.staff_team) in interaction.user.roles:
+        ticket_id = interaction.channel.name.removeprefix("ticket-")
+        await interaction.response.send_message("Successfully deleted ticket (this'll go away it needs to be sent)")
+        delete_ticket(ticket_id)
+    else:
+        await interaction.response.send_message(embed=embeds.insufficient_permissions("STAFF_TEAM"))
     
+async def delete_ticket(id : int | str):
+    t_category = discord.utils.get(client.guilds[0].categories, id=ids.tickets_cat)
+    for c in t_category.channels:
+        if c.name == f"ticket-{id}":
+            await c.delete()
+            return
+    # if it gets here it means the ticket doesnt exist
+
 client.run(config.TOKEN)
--- embeds.py
+++ embeds.py
@@ -3,3 +3,15 @@ import discord
 def message_delete(msg : discord.Message):
     embed = discord.Embed(colour=discord.Colour.red(), title="Message deleted", description=f"User ID: {msg.author.id}\nUser Mention: {msg.author.mention}\n\nContent: {msg.content}")
     return embed
+
+def get_guide(permission : str):
+    if permission.lower() == "staff_team":
+        return "You must be a staff member"
+    if permission.lower() == "manage_roles":
+        return "You need to be able to manage roles"
+    return "No guide has been found for this permission, either the name should imply it or it has not been implemented, Google it"
+
+def insufficient_permissions(permission : str):
+    # Use SPECIFIC names, if you dont, it will never return a guide
+    embed = discord.Embed(colour=discord.Colour.red(), title="Insufficient Permissions", description=f"You do not have the required permission {permission}\n\nGuide:\n{get_guide(permission)}")
+    return embed
